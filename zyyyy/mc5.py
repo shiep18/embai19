07a4e4fde2d9c4dde562b3862eaec79d32702047
Zhangyue0820 <47958691+Zhangyue0820@users.noreply.github.com>
2019-03-13 23:28:02 +0800

    我的世界

---
+++ zyyyy/mc5.py
@@ -0,0 +1,13 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import time
+
+mc = minecraft.Minecraft.create()
+def buildBridge():
+    pos = mc.player.getTilePos()
+    b=mc.getBlock(pos.x,pos.y-1,pos.z)
+    if b==block.AIR.id or b==block.WATER_STATIONARY.id or b==block.WATER_FLOWING.id:
+        mc.setBlock(pos.x,pos.y-1,pos.z,block.GLASS.id)
+
+while True:
+    buildBridge()
